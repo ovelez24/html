8fb62f2a36c91d84736d34995d8cdd863e7f05b4
oscar Velez <ovelez_24@hotmail.com>
2025-06-01 14:11:26 -0500

    cambio 010625

--- javascriptud/dia15/2 - Otras Solicitudes/misScripts.js
+++ javascriptud/dia15/2 - Otras Solicitudes/misScripts.js
@@ -1,4 +1,4 @@
-fetch('https://jsonplaceholder.typicode.com/posts/5', {
+/*fetch('https://jsonplaceholder.typicode.com/posts/5', {
     method: 'PUT',//es un ajuste o modificacion
     headers: {
         'Content-Type': 'application/json'
@@ -10,15 +10,15 @@ fetch('https://jsonplaceholder.typicode.com/posts/5', {
 })
 .then(respuesta => respuesta.json())//esto simplifica un funcion anonima
 .then(data => console.log(data))
-.catch(error => console.error('Error:', error));
+.catch(error => console.error('Error:', error));*/
 
-//Metodo DELETE
-/*fetch('https://jsonplaceholder.typicode.com/posts/5', {
+/*//Metodo DELETE
+fetch('https://jsonplaceholder.typicode.com/posts/5', {
     method: 'DELETE',
     })
 .then(respuesta => respuesta.json())
 .then(data => console.log(data))
-.catch(error => console.error('Error:', error));
+.catch(error => console.error('Error:', error));*/
 
 
 //metodo PATCH
@@ -33,4 +33,4 @@ fetch('https://jsonplaceholder.typicode.com/posts/5', {
 })
 .then(respuesta => respuesta.json())
 .then(data => console.log(data))
-.catch(error => console.error('Error:', error));*/
+.catch(error => console.error('Error:', error));
